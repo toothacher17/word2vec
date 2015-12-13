52c9c750d38be2c063b411f7596bd761f3710af8
toothacher17 <toothacher17@gmail.com>
2015-12-13 17:23:32 -0500

    modify settings

--- deep.sh
+++ deep.sh
@@ -1,3 +1,3 @@
 make
-time ./word2vec -train word_file -output vectors.bin -cbow 1 -size 40 -window 5 -negative 5 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
+time ./word2vec -train data/word_file -output vectors.bin -cbow 1 -size 40 -window 5 -negative 5 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
 ./distance vectors.bin
